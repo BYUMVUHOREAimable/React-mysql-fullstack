93d2552941f2b694642c4ecd505fd1e4f2454bf9
BYUMVUHORE Aimable <aimablebyumvuhore@gmail.com>
2025-02-21 14:33:43 +0200

    Adding create and get routers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const db = require("./models");
 
-db.sequelize.sync().then(() => {  // Use db.sequelize instead of db.Sequelize
+//Routers
+const postsRouter = require("./routes/Posts");
+app.use("/posts", postsRouter);
+
+db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log("Server running on port 3001");
     });
